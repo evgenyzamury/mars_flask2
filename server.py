9ced84735ef7fec1def497e7832f9bc1a69a2397
BERSEK <zamurijevgenyij@gamil.com>
2025-02-26 15:26:13 +0300

    class 4

--- server.py
+++ server.py
@@ -3,9 +3,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+@app.route('/')
+def index():
+    return render_template('base.html', title='')
+
+
 @app.route('/<title>')
 @app.route('/index/<title>')
-def index(title):
+def index_title(title):
     return render_template('base.html', title=title)
 
 
@@ -16,9 +21,25 @@ def training(name):
 
 @app.route('/list_prof/<lst>')
 def list_prof(lst):
-    professions = ['инженер-исследователь', 'пилот', 'строитель', 'экзобиолог', "врач", "инженер по терраформированию"
-        , "климатолог"]
+    professions = ['инженер-исследователь', 'пилот', 'строитель', 'экзобиолог', "врач", "инженер по терраформированию",
+                   "климатолог"]
     return render_template('list_prof.html', lst=lst, professions=professions)
+
+
+@app.route('/answer')
+@app.route('/auto_answer')
+def answer():
+    params = {
+        "title": "Анкета",
+        "surname": "Watny",
+        "name": "Mark",
+        "education": "выше среднего",
+        "proffesion": "штурман марсахода",
+        "sex": "male",
+        "motivation": "Всегда мечтал застрять на Марсе!",
+        "ready": 'True'
+    }
+    return render_template('auto_answer.html', **params)
 
 
 if __name__ == '__main__':
